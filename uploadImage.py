91d42baae19d1148004800d64d360a3265481e05
raphael strebel <raphael.strebel@epfl.ch>
2018-02-24 00:55:02 +0100

    minor changes

--- uploadImage.py
+++ uploadImage.py
@@ -1,14 +1,14 @@
 import requests
 import webbrowser
 import urllib.request
-filePath = '/home/emilio/Desktop/test.jpg'
+filePath = '/Users/raphaelstrebel/Downloads/Hien-Nhon-gate.jpg'
 searchUrl = 'http://www.google.ch/searchbyimage/upload'
 multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
 response = requests.post(searchUrl, files=multipart, allow_redirects=False)
 fetchUrl = response.headers['Location']
-webbrowser.open(fetchUrl)
+#webbrowser.open(fetchUrl)
 
 opener = urllib.request.FancyURLopener({})
 f = opener.open(fetchUrl)
 content = f.read()
-print(content)
+#print(content)
