51444db5d8f0bb0c846e9e650f4c8049faa25cb6
Gilbert Maystre <gilbert@maystre.ch>
2018-02-24 15:13:06 +0100

    Make content extractor work

--- backend/src/ContentExtractor.py
+++ backend/src/ContentExtractor.py
@@ -1,30 +1,19 @@
-#Given an url, this function must return the main content as a string or None if some error was encouteredimport http.client
 import json
 import requests
 import pickle
 import urllib
 
-#urlToAnalyse = "https://www.indiatoday.in/education-today/gk-current-affairs/story/picassos-masterpiece-sold-auction-new-york-977763-2017-05-18"
-
-
 class Website:
 
 	def __init__(self, url):
 		self.url = url
-		self.isArticle = False
-		self.date = ""
-		self.language = "en"
-		self.image = []
-		self.json = self.getJson()
 
-	def getJson(self):
-
+	def fetch(self):
 		token = "be8deae83c43d9f061a2726cc24a315c"
 		apiUrl = "https://api.diffbot.com/v3/analyze?token=" + token + "&url=" + self.url
 		req = urllib.request.Request(apiUrl)
 		r = urllib.request.urlopen(req).read()
-
-		return json.loads(r.decode('utf-8'))
+		self.json = json.loads(r.decode('utf-8'))
 
 	def getText(self):
 		return self.json["objects"][0]["text"]
@@ -39,14 +28,18 @@ class Website:
 		return self.json["humanLanguage"]
 
 	def getImages(self):
-		all_url = []
-		for im in self.json["objects"][0]["images"]:
-			all_url.append(im["url"])
+		return [im["url"] for im in self.json["objects"][0]["images"]]
 
-		return all_url
 
-website = Website("https://www.indiatoday.in/education-today/gk-current-affairs/story/picassos-masterpiece-sold-auction-new-york-977763-2017-05-18")
-print(website.getText())
-print(website.getLanguage())
-print(website.getDate())
-print(website.getImages())
+##########################
+# After here, we unit test
+##########################
+
+if __name__ == "__main__":
+	website = Website("https://www.indiatoday.in/education-today/gk-current-affairs/story/picassos-masterpiece-sold-auction-new-york-977763-2017-05-18")
+	website.fetch()
+	print(website.isArticle())
+	print(website.getText())
+	print(website.getLanguage())
+	print(website.getDate())
+	print(website.getImages())
