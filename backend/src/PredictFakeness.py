b2c2135843ba0b5cf1f6cb58dd1f13b8b89aa3ac
Martin Fontanet <martin.fontanet@epfl.ch>
2018-02-24 15:03:55 +0100

    addon with circle of different colors

--- backend/src/PredictFakeness.py
+++ backend/src/PredictFakeness.py
@@ -1,4 +1,6 @@
+import random
 # returns a prediction dictionary with keys:
 #    isFake in [0, 1]
 def predictFakeness(imagePath, url):
-	return "Defo not a fake dayum for (" + str(imagePath) + ", " + str(url) + ")"
+	return str(random.randint(1,255))
+	#return "Defo not a fake dayum for (" + str(imagePath) + ", " + str(url) + ")"
