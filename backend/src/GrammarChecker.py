37d9f0c3ac1c716c30cc266c095bb4c73be610e1
Gilbert Maystre <gilbert@maystre.ch>
2018-02-24 04:32:15 +0100

    Implemented spell checker

--- backend/src/GrammarChecker.py
+++ backend/src/GrammarChecker.py
@@ -1,2 +1,9 @@
+import requests
 
 def isSpellingOk(text):
+	r = requests.get(prepareRequest(text))
+	response = r.json()
+	return len(response["matches"]) == 0
+
+def prepareRequest(text):
+	return "http://localhost:8082/v2/check?language=en-US&text=" + text
