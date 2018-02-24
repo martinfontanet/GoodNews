2334b8f925a330be5d2961e033417cb4cdb52606
raphael strebel <raphael.strebel@epfl.ch>
2018-02-24 14:42:15 +0100

    content extractor done

--- backend/src/ContentExtractor.py
+++ backend/src/ContentExtractor.py
@@ -1,3 +1,52 @@
-#Given an url, this function must return the main content as a string or None if some error was encoutered
-def extractMainContent(link):
-	return "todo"
+#Given an url, this function must return the main content as a string or None if some error was encouteredimport http.client
+import json
+import requests
+import pickle
+import urllib
+
+#urlToAnalyse = "https://www.indiatoday.in/education-today/gk-current-affairs/story/picassos-masterpiece-sold-auction-new-york-977763-2017-05-18"
+
+
+class Website:
+
+	def __init__(self, url):
+		self.url = url
+		self.isArticle = False
+		self.date = ""
+		self.language = "en"
+		self.image = []
+		self.json = self.getJson()
+
+	def getJson(self):
+
+		token = "be8deae83c43d9f061a2726cc24a315c"
+		apiUrl = "https://api.diffbot.com/v3/analyze?token=" + token + "&url=" + self.url
+		req = urllib.request.Request(apiUrl)
+		r = urllib.request.urlopen(req).read()
+
+		return json.loads(r.decode('utf-8'))
+
+	def getText(self):
+		return self.json["objects"][0]["text"]
+
+	def isArticle(self):
+		return self.json["objects"][0]["type"] == "article"
+
+	def getDate(self):
+		return self.json["objects"][0]["date"]
+
+	def getLanguage(self):
+		return self.json["humanLanguage"]
+
+	def getImages(self):
+		all_url = []
+		for im in self.json["objects"][0]["images"]:
+			all_url.append(im["url"])
+
+		return all_url
+
+website = Website("https://www.indiatoday.in/education-today/gk-current-affairs/story/picassos-masterpiece-sold-auction-new-york-977763-2017-05-18")
+print(website.getText())
+print(website.getLanguage())
+print(website.getDate())
+print(website.getImages())
